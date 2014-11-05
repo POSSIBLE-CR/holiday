5cdb02999951cf94b5e83cee63df9af108b63cdd
Julio Del Valle G. <juulio@gmail.com>
2014-11-05 15:17:10 -0600

    HOLIPRO-1:: PS1 update

--- provision.sh
+++ provision.sh
@@ -35,7 +35,7 @@ echo "c2VydmVyIHsNCglzZXJ2ZXJfbmFtZSAxOTIuMTY4LjE2OC4xNjg7DQogICAgICAgIGxvY2F0aW
 service nginx restart
 
 # Config files
-echo 'export PS1="\[\e[01;32m\]\[\e[1m\][\[\e[0m\]\t\[\e[01;32m\]\[\e[1m\]]\[\e[0m\]\[\e[1m\] \w:>\[\e[0m\] "' | tee -a ~vagrant/.bashrc
+echo 'exportZXhwb3J0IFBTMT0iXFtcZVswMTszMm1cXVxbXGVbMW1cXVtcW1xlWzBtXF1cdFxbXGVbMDE7MzJtXF1cW1xlWzFtXF1dXFtcZVswbVxdXFtcZVsxbVxdIFx3Oj5cW1xlWzBtXF0gIg==' | base64 -d | tee -a ~vagrant/.bashrc
 echo 'IlxlW0EiOiAgICAgaGlzdG9yeS1zZWFyY2gtYmFja3dhcmQNCiJcZVtCIjogICAgIGhpc3Rvcnktc2VhcmNoLWZvcndhcmQNCiJcZU9BIjogICAgIGhpc3Rvcnktc2VhcmNoLWJhY2t3YXJkDQoiXGVPQiI6ICAgICBoaXN0b3J5LXNlYXJjaC1mb3J3YXJkDQo=' | base64 -d | tee -a /etc/inputrc
 
 # Owner

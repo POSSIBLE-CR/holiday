d2fe68c213a21e46a107d5e09176cc04aa298124
Julio Del Valle <juulio@gmail.com>
2014-11-05 11:20:30 -0600

    HOLIPRO-1:: Adding basic folder structure and files

--- controllers/home.js
+++ controllers/home.js
@@ -2,7 +2,7 @@ var express = require('express');
 var home = express.Router();
 
 home.get('/', function(req, res, next) {
-    res.send('hello world');
+    res.render('home');
 });
 
 module.exports.home = home;

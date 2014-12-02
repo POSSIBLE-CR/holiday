ea9d1806907c9c25b88055921386d50b148e72ce
Gloriana Zamora <glorianazv@gmail.com>
2014-12-02 10:22:28 -0600

    HOLIPRO-19 :: [DEV][FE] Map Visualization

--- controllers/home.js
+++ controllers/home.js
@@ -5,4 +5,12 @@ home.get('/', function(req, res, next) {
     res.render('home');
 });
 
+home.get('/geo', function(req, res, next) {
+    res.render('/geo');
+});
+
+home.get('/map', function(req, res, next) {
+    res.render('map');
+});
+
 module.exports.home = home;

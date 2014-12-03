c199b2e16fd44fa4fd2efc5b4dd4b6feaf85f0c1
Kenneth Brenes <kenneth.brenes@kbrenes-3414.local>
2014-12-03 10:41:50 -0600

    Adding cookie logic for sign in /fb/tw

--- public/js/base.js
+++ public/js/base.js
@@ -1,4 +1,47 @@
-var a = "hola";
+function setCookie(cookie){
+
+	if(cookie == "tw"){
+		$.cookie("social_login", "tw");
+	}
+	else{
+		$.cookie("social_login", "fb");
+	}
+}
+
+var base = base || {};
+
+base.party = (function ($) {
+
+
+	function init() {
+
+        $('.hola').on('click', function(){
+              
+                          
+        });
+    }
+	
+	return {
+        init: init
+    };
+
+})(jQuery);
+base.party.init();
+
+
+/*
+$('.hola').on('click', function(e){
+    e.preventdefault;
+    console.log('Test');
+    			
+    return false;             
+});
+
+$( ".hola" ).click(function() {   
+    return false;
+});
+*/  
+/*
 
 $( ".btn icon-twitter" ).click(function() {
     alert("tw");
@@ -9,9 +52,14 @@ $( ".btn icon-facebook" ).click(function() {
     alert("fb");
     $.cookie("social_login", "fb");
 });    
+*/
 
 
-$( "#hola" ).click(function() {
-    console.log('ww');
-    alert("fb");
-});  
+function hola(){
+	
+	console.log(document.cookie);
+	$.removeCookie('social_login', { path: '/' });
+	console.log(document.cookie);
+
+}
+

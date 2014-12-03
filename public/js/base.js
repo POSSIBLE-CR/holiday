d6ed445d06c4f3f62295a1de509664be5fa29aef
Kenneth Brenes <kenneth.brenes@kbrenes-3414.local>
2014-12-02 19:23:15 -0600

    Adding cookie logic for sign in

--- public/js/base.js
+++ public/js/base.js
@@ -1,1 +1,17 @@
 var a = "hola";
+
+$( ".btn icon-twitter" ).click(function() {
+    alert("tw");
+    $.cookie("social_login", "tw");
+});    
+
+$( ".btn icon-facebook" ).click(function() {
+    alert("fb");
+    $.cookie("social_login", "fb");
+});    
+
+
+$( "#hola" ).click(function() {
+    console.log('ww');
+    alert("fb");
+});  
